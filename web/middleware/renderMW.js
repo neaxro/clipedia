b9755522fa8aafd114c12e99a00f67de4c77e090
Axel Roland Nemes <neaxro@gmail.com>
2025-03-22 13:39:12 +0100

    Added dynamic behaviour for command listing Added extra logic to handle user is logged in or not.

--- web/middleware/renderMW.js
+++ web/middleware/renderMW.js
@@ -6,6 +6,7 @@ const requireOption = require('./requireOption');
 
 module.exports = function(objectrepository, viewName) {
     return function(req, res) {
+        res.locals.session = req.session;
         res.render(viewName);
     };
 };
