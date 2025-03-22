d6d52dd8bdcf3bb0cf41da6e6bb26435d63b4b8c
Axel Roland Nemes <neaxro@gmail.com>
2025-03-22 14:21:57 +0100

    Added dynamic behaviour to etail command page.

--- web/middleware/commands/getCommandMW.js
+++ web/middleware/commands/getCommandMW.js
@@ -8,6 +8,23 @@ module.exports = function(objectrepository) {
         // TODO: Implement db call for fecthing command detail.
 
         const commandName = req.params.commandname;
+        const dummy_command = {
+            id: 1,
+            name: "apt",
+            description: "Package management command for installing, updating, and removing software.",
+            examples: [
+                "sudo apt update",
+                "sudo apt install <package>",
+                "sudo apt remove <package>"
+            ],
+            links: [
+                "https://manpages.debian.org/buster/apt/apt.8.en.html"
+            ],
+            badges: ["Package Management"],
+            group_id: 1 // Debian
+        };
+
+        res.locals.command = dummy_command;
 
         console.log("[DETAIL] Command name is " + commandName);
 
