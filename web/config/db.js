414a26da10a35496ff011d60bc85b1286cecc917
Axel Roland Nemes <neaxro@gmail.com>
2025-04-15 23:09:49 +0200

    Added auto test data load for mongodb.

--- web/config/db.js
+++ web/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://root:pass@localhost:27017/clipedia', {
+mongoose.connect('mongodb://root:pass@mongodb:27017/clipedia', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     authSource: "admin"
